5fe5434625afb148616eeb848d89633f223f4e9b
Narayan Desai <desai@mcs.anl.gov>
2011-05-16 17:02:39 -0500

    support for command line flags

--- simpleclientmain.go
+++ simpleclientmain.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+    "flag"   
+    "fmt"
     "os"
     "./simpleclient"
 )
 
+var Usage = func() {
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+    //PrintDefaults()
+}
+
+var server string
+
+func init() {
+     flag.StringVar(&server, "S", "http://localhost:8081", "server base URL")
+}
+
 func main() {
-    bc := simpleclient.NewBuildClient("fred")
+    flag.Parse()
+    fmt.Fprintf(os.Stderr, "Server is %s\n", server)
+    bc := simpleclient.NewBuildClient(server)
     host := (*bc).GetHostname()
     os.Stdout.WriteString(host + "\n")
     data := (*bc).Get("dags")
