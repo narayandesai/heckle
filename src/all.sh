66b1e144c9a1e136b52b9338c46eb72f47a04e77
Levi Lester <gauge2@gmail.com>
2011-07-20 14:08:06 -0500

    added flunkymasterd to all.sh

--- src/all.sh
+++ src/all.sh
@@ -6,6 +6,6 @@ mk () {
     popd
 }
 
-for target in pkg/daemon pkg/net pkg/interfaces cmd/flunky cmd/fctl cmd/diagd cmd/powerd cmd/heckled cmd/testHeckle; do
+for target in pkg/daemon pkg/net pkg/interfaces cmd/flunky cmd/fctl cmd/diagd cmd/powerd cmd/heckled cmd/testHeckle cmd/flunkymasterd; do
     mk $target $1
 done
