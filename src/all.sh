3d37e96bc22e728e95737345e33f2553c629f7a3
Narayan Desai <desai@mcs.anl.gov>
2011-07-20 21:31:27 -0500

    build change (reduce verbosity)

--- src/all.sh
+++ src/all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mk () {
-    pushd $1
+    pushd $1 >/dev/null && echo $1
     gomake $2
-    popd
+    popd >/dev/null
 }
 
 for target in pkg/daemon pkg/net pkg/interfaces cmd/flunky cmd/fctl cmd/diagd cmd/powerd cmd/heckled cmd/testHeckle cmd/flunkymasterd; do
