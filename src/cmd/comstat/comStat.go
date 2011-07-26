acca4d09450545a3380b752f1ca0df660d30fb8c
Michael Guantonio <mike.guantonio@gmail.com>
2011-07-26 14:49:10 -0500

    Fixed the comstat.go

--- src/cmd/comstat/comStat.go
+++ src/cmd/comstat/comStat.go
@@ -4,8 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"json"
+	"io/ioutil"
 	inet "flunky/net"
-	daemon "flunky/daemon"
 )
 
 var Usage = func() {
@@ -16,16 +17,40 @@ var Usage = func() {
 
 var help bool
 var fileDir string
-var comStatDaemon *daemon.Daemon
 
 func init() {
+	fileDir = "../../../etc/Heckle/"
 	flag.BoolVar(&help, "h", false, "Print help message")
 }
 
-func GetDump(daemonName string) {
+func ReadConfig(fname string) (comData map[string]string) {
+	var contents []byte
+	_, err := os.Stat(fname)
+	if err != nil {
+		fmt.Println(fname + " does not exsist")
+	} else {
+		contents, err = ioutil.ReadFile(fname)
+		if err != nil {
+			fmt.Println("Cannot read file")
+		}
+	}
 
-	server, ok := comStatDaemon.Cfg.Data[daemonName]
-        fmt.Println("HErE")
+	if len(contents) > 0 {
+		err := json.Unmarshal(contents, &comData)
+		if err != nil {
+			fmt.Println("Unable to unmarshal data")
+		}
+	} else {
+		fmt.Println("Invalid json. Could not read data")
+	}
+	return
+
+}
+
+func GetDump(daemonName string) {
+	config := ReadConfig(fileDir)
+	server, ok := config[daemonName]
+
 	if !ok {
 		fmt.Println(fmt.Sprintf("Cannot find URL for component %s", daemonName))
 		os.Exit(1)
@@ -49,8 +74,14 @@ func main() {
 		Usage()
 		os.Exit(0)
 	}
-        fmt.Println(flag.Args())
-	for _, name := range flag.Args() {
-		GetDump(name)
+
+	if len(os.Args) <= 1 {
+		fmt.Println("No arguements provided")
+		Usage()
+		os.Exit(1)
+	} else {
+		for _, name := range flag.Args() {
+			GetDump(name)
+		}
 	}
 }
