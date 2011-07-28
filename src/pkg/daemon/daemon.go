f7730a4ff3f7f0f9c503ec352c69f0125d9a2844
Michael Guantonio <mike.guantonio@gmail.com>
2011-07-28 10:29:34 -0500

    MASTER: fixed error in daemon.go to allow for port parsing. len(part) is not the same as len(part) -1

--- src/pkg/daemon/daemon.go
+++ src/pkg/daemon/daemon.go
@@ -29,7 +29,8 @@ type Daemon struct {
 func (daemon *Daemon) GetPort() (port string, err os.Error) {
 	parts := strings.Split(daemon.URL, ":")
 	if len(parts) > 0 {
-		port = parts[len(parts)]
+		port = parts[len(parts)-1]
+		fmt.Println(port)
 		return
 	}
 	err = os.NewError("Failed to parse URL")
