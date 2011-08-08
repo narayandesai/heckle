7474c920883348bfef1540bd8b8f5eb2735f4a40
Michael Guantonio <mike.guantonio@gmail.com>
2011-08-08 11:19:37 -0500

    MASTER: started tracking common.go and flunky.conf

---
+++ src/pkg/client/common.go
@@ -0,0 +1,20 @@
+package client
+
+import(
+	"flag"
+	"os"
+	"fmt"
+)
+
+func Usage(){
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+    flag.PrintDefaults()
+
+}
+
+func PrintError(message string, err os.Error ){
+     if err != nil {
+        fmt.Fprintf(os.Stderr, "%s\n", message)
+     }
+     return
+}
