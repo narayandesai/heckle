84c9a07ca8b9d838e5c7e3c4d4eb893de29b71e4
Michael Guantonio <mike.guantonio@gmail.com>
2011-08-12 17:23:07 -0500

    MASTER: Added log lookup ability in comstat.go

--- src/cmd/comstat/comStat.go
+++ src/cmd/comstat/comStat.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"io/ioutil"
 	fclient "flunky/client"
 )
 
 var help bool
 var fileDir string
+var error bool
+var log bool
 
 func init() {
 	flag.BoolVar(&help, "h", false, "Print help message")
+	flag.BoolVar(&error, "e", false, "Print error logs")
+	flag.BoolVar(&log, "l", false, "Print log file for specified host")
 }
 
 func prettyPrint(data string) {
@@ -22,6 +27,31 @@ func prettyPrint(data string) {
      }
      return
 
+}
+
+func logs(name string) string {
+   var errorLog string
+   logName := "/var/log/" + name + ".log"
+   _,err :=  os.Stat(logName)
+   if err != nil{
+       fclient.PrintError(err.String(), err)
+       os.Exit(1)
+   }
+   file, err := ioutil.ReadFile(logName)
+   if log{
+      return string(file)
+   }
+   if error {
+      contents := strings.Split(string(file), "\n")
+      for _, data := range(contents){
+      	   dex := strings.Index(data, "ERROR")
+       	   if dex > 0{
+              errorLog = errorLog + data + "\n"
+       	    }
+       }
+   return(errorLog)
+   }
+   return ""
 }
 
 func main() {
@@ -39,6 +69,26 @@ func main() {
 		fclient.Usage()
 		os.Exit(1)
 	} else {
+	        if log{
+		   for _, name := range(flag.Args()){
+		       logging := logs(name)
+		       fmt.Println(logging)
+                   }
+		   os.Exit(0)
+                 }
+   
+                 if error {
+		    for _, name := range(flag.Args()){
+		        errorLogging := logs(name)
+			if len(errorLogging) <=0{
+	                     fmt.Println(fmt.Sprintf("No errors exsist in the logs for %s", name))
+                        }else{
+				fmt.Println(errorLogging)
+                        }
+                     }
+		     os.Exit(0)
+                  }
+		    
 		comm, err := fclient.NewClient()
 		if err != nil {
 			fclient.PrintError("Failed to setup communication", err)
